58bdb826c377713f4b2c2b27bef0856056d6c6aa
drdanick <di.da.nick@gmail.com>
2016-12-09 08:11:20 -0800

    Program will now display a blank window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "apricosterm.h"
+#include "screen.h"
 
 int main(int argc, char** argv) {
+    Color c = {255,255,255};
+    SDL_Event event;
+    initScreen("Potato", 640, 480, c);
+    char done = 0;
+
+    while(!done) {
+        while(SDL_PollEvent(&event)) {
+            switch(event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    done = 1;
+                    break;
+            }
+        }
+        SDL_Delay(100);
+        updateWindow();
+    }
+    
     return EXIT_SUCCESS;
 }
