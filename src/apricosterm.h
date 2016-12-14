6a1cf11567738c3290a366c59d228a231ab69889
drdanick <di.da.nick@gmail.com>
2016-12-14 15:41:32 -0800

    Added global terminal settings

--- src/apricosterm.h
+++ src/apricosterm.h
@@ -3,4 +3,17 @@
 
 #include <SDL2/SDL.h>
 
+/* Font info */
+#define FONT_FILE   "vga8x16.png"
+#define CHAR_WIDTH  8
+#define CHAR_HEIGHT 16
+#define FONT_COLS   32
+#define FONT_LINES  8
+
+/* Screen info*/
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
+#define SCREEN_ROWS   (SCREEN_HEIGHT / CHAR_HEIGHT)
+#define SCREEN_COLS   (SCREEN_WIDTH / CHAR_WIDTH)
+
 #endif /* APRTERM_H */
