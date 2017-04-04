9e8d48b12f61009fe0f7f549845e3765cc361c9e
drdanick <di.da.nick@gmail.com>
2017-04-04 02:30:49 -0700

    Sleep a little longer if there are no input bytes

--- src/main.c
+++ src/main.c
@@ -86,6 +86,8 @@ int main(int argc, char** argv) {
             termbyte = termioRead();
             if(termbyte > -1)
                 terminalHandleByte((char)termbyte);
+            else
+                SDL_Delay(10);
         }
         terminalRefresh();
     }
